bd82233bbfe5d706b390a0a0600afc9169450c82
Subhranshu Das <subhranshu@Subhranshus-MacBook-Pro.local>
2022-04-19 18:36:26 +0530

    added some mType

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     Helpers.pusblishMsgToSDK(
-      Helpers.createMsgPayload('EMBED APP LOADED...')
+      Helpers.createMsgPayload({ msg: ' IFRAME sAPP loaded', msgType: 'IFRAME_APP_LOADED' })
     );      
   }, []);
 
--- src/helpers.js
+++ src/helpers.js
@@ -1,9 +1,10 @@
 const Helpers = {
-  createMsgPayload(msg) {
+  createMsgPayload({ msg, ...otherParams}) {
     return {
         msg,
         timestamp: Date.now(),
-        msgCode: 'EPNS_SDK_IFRAME_TO_PARENT_MSG' // this has to be picked from the SDK
+        msgCode: 'EPNS_SDK_IFRAME_TO_PARENT_MSG', // this has to be picked from the SDK
+        ...otherParams 
     };
   },
   pusblishMsgToSDK(msgPayload) {
